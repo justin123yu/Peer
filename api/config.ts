2a7292fb7519862d135a8ceed707cc261465fe40
Justin Yu <justin741yu@gmail.com>
2025-04-28 16:26:57 -0700

    Typing for env

--- api/config.ts
+++ api/config.ts
@@ -1,3 +1,13 @@
+interface Env {
+  OPENAI_API_KEY: string;
+  QDRANT_API_KEY: string;
+  QDRANT_URL?: string;
+}
+
+declare const process: {
+  env: Env;
+};
+
 export const config = {
   openai: {
     apiKey: process.env.OPENAI_API_KEY,
